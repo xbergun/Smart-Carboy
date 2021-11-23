79324d3cc4685b94e14bc168542ad0c0a201ad75
farukmeral25 <farukmeral25@gmail.com>
2021-11-24 01:22:36 +0300

    Model

---
+++ lib/feature/home/domain/entity/water_carboy.dart
@@ -0,0 +1,11 @@
+class WaterCarboy {
+  final String title;
+  final String imagePath;
+  int waterNumber;
+
+  WaterCarboy({
+    required this.title,
+    required this.imagePath,
+    this.waterNumber = 0,
+  });
+}
