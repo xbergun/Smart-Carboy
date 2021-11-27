f6cf30fc83518592b7a5328e95980ee9bee6dac1
farukmeral25 <farukmeral25@gmail.com>
2021-11-27 22:28:26 +0300

    add removeFocus funciton

---
+++ lib/core/sharedWidgets/remove_focus.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void removeFocus(context) {
+  FocusScopeNode currentFocus = FocusScope.of(context);
+  if (!currentFocus.hasPrimaryFocus) {
+    currentFocus.unfocus();
+  }
+}
