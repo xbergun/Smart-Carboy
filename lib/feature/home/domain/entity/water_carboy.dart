607240e2f359fbf58e80349c2b18e4ca9a76c650
farukmeral25 <farukmeral25@gmail.com>
2021-11-27 04:11:12 +0300

    Price added to model

--- lib/feature/home/domain/entity/water_carboy.dart
+++ lib/feature/home/domain/entity/water_carboy.dart
@@ -1,10 +1,12 @@
 class WaterCarboy {
   final String title;
+  final double price;
   final String imagePath;
   int waterNumber;
 
   WaterCarboy({
     required this.title,
+    required this.price,
     required this.imagePath,
     this.waterNumber = 0,
   });
