25f45ae93957075eeb1b3a397c51f7f27eeb96ed
farukmeral25 <farukmeral25@gmail.com>
2021-11-27 04:09:40 +0300

    add AppTextStyle

--- lib/core/constant/app_text_styles.dart
+++ lib/core/constant/app_text_styles.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 class AppTextStyles {
   static TextStyle panton24Px950 = TextStyle(
     color: AppColors.lightBlue,
-    fontSize: ScreenSize().dynamicWidth(.05),
+    fontSize: ScreenSize().dynamicWidth(.06),
     fontWeight: FontWeight.w900,
     fontStyle: FontStyle.normal,
   );
@@ -15,6 +15,25 @@ class AppTextStyles {
     color: AppColors.lightBlue,
     fontSize: ScreenSize().dynamicWidth(.07),
     fontWeight: FontWeight.w900,
+    fontStyle: FontStyle.normal,
+  );
+
+  static TextStyle roboto20Px900 = TextStyle(
+    color: AppColors.lightBlue,
+    fontSize: ScreenSize().dynamicWidth(.05),
+    fontWeight: FontWeight.w900,
+    fontStyle: FontStyle.normal,
+  );
+  static TextStyle roboto16Px700 = TextStyle(
+    color: AppColors.lightBlue,
+    fontSize: ScreenSize().dynamicWidth(.04),
+    fontWeight: FontWeight.w700,
+    fontStyle: FontStyle.normal,
+  );
+  static TextStyle roboto12Px400 = TextStyle(
+    color: AppColors.lightBlue,
+    fontSize: ScreenSize().dynamicWidth(.03),
+    fontWeight: FontWeight.w400,
     fontStyle: FontStyle.normal,
   );
 }
